444d44a460c887bbef3021ef70080146c8822abd
Daniel Chaiken <daniel.chaiken@system1.bio>
2018-05-31 16:15:21 -0700

    added real files

---
+++ RandomModule.py
@@ -0,0 +1,9 @@
+"""
+Random module for CircleCI tests
+"""
+
+class RandomModule:
+
+    @staticmethod
+    def adder(a=0,b=0):
+        return a + b
---
+++ RandomModule_test.py
@@ -0,0 +1,4 @@
+from RandomModule import RandomModule
+
+def test_adder():
+    assert RandomModule.adder(1,5) == 6
