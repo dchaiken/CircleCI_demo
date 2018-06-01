0475db621685844c0db7803eaad947a24c493296
Daniel Chaiken <daniel.chaiken@system1.bio>
2018-06-01 13:20:31 -0700

    test_that_fails works now

--- RandomModule_test.py
+++ RandomModule_test.py
@@ -2,6 +2,7 @@ from RandomModule import RandomModule
 
 def test_adder():
     assert RandomModule.adder(1,5) == 6
+    assert RandomModule.adder("Hello ", "there!")== "Hello there!"
 
 def test_that_fails():
-    assert False
+    assert True
